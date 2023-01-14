94a5fff5e2836f7ef291288741bf7c1fb755c15f
Kurt Aadnøy <kurt@aadnoy.info>
2023-01-14 10:16:23 +0100

    Fix scroll macros

--- routines/scroll.asm
+++ routines/scroll.asm
@@ -1,12 +1,41 @@
-.var yScroll=$d011
-.var xScroll=$d016
+.var y_scroll=$d011
+.var x_scroll=$d016
 
 .macro ScrollLeft() {
-    lda xScroll,x
-    adc #01
-    cmp #$08
-    beq resetX
-    sta xScroll
+    lda x_scroll
+    sec
+    sbc #$01
+    and #$07
+    sta $fb
+    //sta pixel_offset
+
+    // load current scroll
+    lda x_scroll
+    // remove current scroll offset
+    and #%11111000
+    // set new scroll offset keeping other bit values
+    clc
+    adc $fb
+    
+    sta x_scroll
+
+    // shift text when jumping back to 7
+    and #$07
+    cmp #$07
 }
 
+.macro SaveLeftOverflowChar(x, y, i) {
+    lda x+($28*i)
+    sta y+i
+}
+
+.macro InsertOverflowCharRight(x, y, i) {
+    lda y+i
+    sta x+($28*i)+$27
+}
+
+.macro MoveCharLeft(y) {
+    lda $0400+($28*y)+1,x
+    sta $0400+($28*y),x
+}
 
--- scroller.asm
+++ scroller.asm
@@ -1,8 +1,6 @@
-//#import "routines/scroll.asm"
-:BasicUpstart2(main)
+#import "routines/scroll.asm"
 
-.var y_scroll=$d011
-.var x_scroll=$d016
+:BasicUpstart2(main)
 
 .var continue_basic=$ea31
 .var continue_short=$ea81
@@ -50,22 +48,8 @@ main:
 scroll:
     lda #$00
     sta $d020
-    // increment offset
-    lda pixel_offset
-    adc #$01
-    and #$07
-    sta pixel_offset
-
-    // load current scroll
-    lda x_scroll
-    // remove current scroll offset
-    and #%11111000
-    // set new scroll offset keeping other bit values
-    adc pixel_offset
     
-    sta x_scroll
-
-    and #$07
+    ScrollLeft()
     beq shift_text
 continue:
     // ; acknowledge the interrupt by clearing the VIC's interrupt flag
@@ -74,23 +58,97 @@ continue:
     sta $d020
     jmp continue_short
 shift_text:
-    //inc $d021
+    SaveLeftOverflowChar($0400, $5400, 0)
+    SaveLeftOverflowChar($0400, $5400, 1)
+    SaveLeftOverflowChar($0400, $5400, 2)
+    SaveLeftOverflowChar($0400, $5400, 3)
+    SaveLeftOverflowChar($0400, $5400, 4)
+    SaveLeftOverflowChar($0400, $5400, 5)
+    SaveLeftOverflowChar($0400, $5400, 6)
+    SaveLeftOverflowChar($0400, $5400, 7)
+    SaveLeftOverflowChar($0400, $5400, 8)
+    SaveLeftOverflowChar($0400, $5400, 9)
+    SaveLeftOverflowChar($0400, $5400, 10)
+    SaveLeftOverflowChar($0400, $5400, 11)
+    SaveLeftOverflowChar($0400, $5400, 12)
+    SaveLeftOverflowChar($0400, $5400, 13)
+    SaveLeftOverflowChar($0400, $5400, 14)
+    SaveLeftOverflowChar($0400, $5400, 15)
+    SaveLeftOverflowChar($0400, $5400, 16)
+    SaveLeftOverflowChar($0400, $5400, 17)
+    SaveLeftOverflowChar($0400, $5400, 18)
+    SaveLeftOverflowChar($0400, $5400, 19)
+    SaveLeftOverflowChar($0400, $5400, 20)
+    SaveLeftOverflowChar($0400, $5400, 21)
+    SaveLeftOverflowChar($0400, $5400, 22)
+    SaveLeftOverflowChar($0400, $5400, 23)
+    SaveLeftOverflowChar($0400, $5400, 24)
+    SaveLeftOverflowChar($0400, $5400, 25)
+
     ldx #$00
-    lda char
-copy_char: 
-    sta $0400,x
-    sta $0500,x
-    sta $0600,x
-    sta $0700,x
+loop1: 
+    MoveCharLeft(0)
+    MoveCharLeft(1)
+    MoveCharLeft(2)
+    MoveCharLeft(3)
+    MoveCharLeft(4)
+    MoveCharLeft(5)
+    MoveCharLeft(6)
+    MoveCharLeft(7)
+    MoveCharLeft(8)
+    MoveCharLeft(9)
+    MoveCharLeft(10)
+    MoveCharLeft(11)
+    MoveCharLeft(12)
+    MoveCharLeft(13)
     inx
-    bne copy_char
+    cpx #$27
+    bne loop1
     
-    inc char
-    lda char
-    cmp end_char
-    bne done
-    lda start_char
-    sta char
+    ldx #$00 
+loop2:
+    MoveCharLeft(14)
+    MoveCharLeft(15)
+    MoveCharLeft(16)
+    MoveCharLeft(17)
+    MoveCharLeft(18)
+    MoveCharLeft(19)
+    MoveCharLeft(20)
+    MoveCharLeft(21)
+    MoveCharLeft(22)
+    MoveCharLeft(23)
+    MoveCharLeft(24)
+    MoveCharLeft(25)
+    inx
+    cpx #$27
+    bne loop2
+    
+    InsertOverflowCharRight($0400, $5400, 0)
+    InsertOverflowCharRight($0400, $5400, 1)
+    InsertOverflowCharRight($0400, $5400, 2)
+    InsertOverflowCharRight($0400, $5400, 3)
+    InsertOverflowCharRight($0400, $5400, 4)
+    InsertOverflowCharRight($0400, $5400, 5)
+    InsertOverflowCharRight($0400, $5400, 6)
+    InsertOverflowCharRight($0400, $5400, 7)
+    InsertOverflowCharRight($0400, $5400, 8)
+    InsertOverflowCharRight($0400, $5400, 9)
+    InsertOverflowCharRight($0400, $5400, 10)
+    InsertOverflowCharRight($0400, $5400, 11)
+    InsertOverflowCharRight($0400, $5400, 12)
+    InsertOverflowCharRight($0400, $5400, 13)
+    InsertOverflowCharRight($0400, $5400, 14)
+    InsertOverflowCharRight($0400, $5400, 15)
+    InsertOverflowCharRight($0400, $5400, 16)
+    InsertOverflowCharRight($0400, $5400, 17)
+    InsertOverflowCharRight($0400, $5400, 18)
+    InsertOverflowCharRight($0400, $5400, 19)
+    InsertOverflowCharRight($0400, $5400, 20)
+    InsertOverflowCharRight($0400, $5400, 21)
+    InsertOverflowCharRight($0400, $5400, 22)
+    InsertOverflowCharRight($0400, $5400, 23)
+    InsertOverflowCharRight($0400, $5400, 24)
+    InsertOverflowCharRight($0400, $5400, 25)
 done:
     jmp continue
 disable_cursor:
